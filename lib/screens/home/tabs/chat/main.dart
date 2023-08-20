9481d5b624c45f5d01cc35c27367e2dc5864d5f2
hackermasterlord <126839836+hackermasterlord@users.noreply.github.com>
2023-08-20 17:53:31 +0700

    Update BottomNavbar + Add change tab

--- lib/screens/home/tabs/chat/main.dart
+++ lib/screens/home/tabs/chat/main.dart
@@ -2,8 +2,8 @@ import 'package:application/widgets/main.dart';
 import 'package:application/screens/home/widgets/navbar.dart';
 import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget {
-  const ChatScreen({super.key});
+class ChatTab extends StatelessWidget {
+  const ChatTab({super.key});
 
   @override
   Widget build(BuildContext context) {
